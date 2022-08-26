9e2c6c879171d370d3c1319a31c430a3c4a279a8
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-08-26 19:18:03 -0300

    Requisito 2 finalizado

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const filteredNames = species.find((names) => names.name === animal);
+  return filteredNames.residents.every((selected) => selected.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
