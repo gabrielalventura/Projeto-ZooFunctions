120b4e625d4626db4867748b7c7abd6a98669aa6
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-08-26 16:35:07 -0300

    Resolvendo lint

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,10 +1,7 @@
-const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  const choosedSpecie = species;
-  const animalAge = species.resident;
-  Object.values(species).every((Specie, Age) => choosedSpecie.name === animal && animalAge.age >= age);
+  // seu código aqui
 }
 
 module.exports = getAnimalsOlderThan;
