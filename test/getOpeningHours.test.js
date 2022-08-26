3dcc4afb9fe8470e179b204013c50ccf7fa2fffc
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-08-26 17:04:13 -0300

    Iniciando o requisito 12

--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,7 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
-describe('Testes da função getOpeningHours', () => {});
+describe('Testes da função getOpeningHours', () => {
+  it('Caso a função receba como parametros `Monday` e `09:00-AM` deve retornar a string `The zoo is closed`', () => {
+    expect(getOpeningHours('Monday', '09:00-AM')).toBe('The zoo is closed');
+  });
+});
