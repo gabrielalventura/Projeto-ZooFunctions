0597aaf5844d8aefa0720189083a453e49cf6872
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-08-26 18:57:51 -0300

    Requisito 1 finalizado

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...choosedIds) {
+  return species.filter((animals) => choosedIds.includes(animals.id));
 }
 
 module.exports = getSpeciesByIds;
+
+// Raciocinio desenvolvido com auxilio da mentoria da Nat Siqueira;
