62ef11de4723b5e8c9a844e8baa0060c6a04efdd
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-08-26 16:07:46 -0300

    Iniciando o requisito 6

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,8 +1,10 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  Object.values(animal).every((age) => age >= 5);
-};
-
+  const choosedSpecie = species;
+  const animalAge = species.resident;
+  Object.values(species).every((Specie, Age) => choosedSpecie.name === animal && animalAge.age >= age);
+}
 
 module.exports = getAnimalsOlderThan;
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,7 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Caso param receba `count` deve retornar 4', () => {
+    expect(handlerElephants('count')).toEqual(4);
+  });
+});
