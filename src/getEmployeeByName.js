580faeb5dbb305b009fe04568716c104c7ed6307
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-08-29 14:21:48 -0300

    Requisito 3 finalizado

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
-  // seu código aqui
+function getEmployeeByName(selectedEmployee) {
+  const notAEmployee = {};
+
+  if (selectedEmployee === undefined) {
+    return notAEmployee;
+  }
+  return employees.find((seekedName) =>
+    selectedEmployee === seekedName.firstName || selectedEmployee === seekedName.lastName);
 }
 
 module.exports = getEmployeeByName;
