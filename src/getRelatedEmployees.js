cd467226ee8013b18b2f7b3f284105a3cf8afa66
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-08-29 16:23:55 -0300

    Requisito 4 finalizado

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,14 +2,14 @@ const data = require('../data/zoo_data');
 const { employees } = require('../data/zoo_data');
 
 function isManager(employeeId) {
-  return employees.some((employee) => employee.id === employeeId && employee.managers === []);
+  return employees.some((employee) => employee.id === employeeId && employee.managers.length <= 1);
 }
 
 function getRelatedEmployees(managerId) {
   if (isManager(managerId) === false) {
     throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
   }
-  return employees.find((element) => element.managers.includes(managerId))
+  return employees.filter((element) => element.managers.includes(managerId))
     .map((element) => `${element.firstName} ${element.lastName}`);
 }
 
