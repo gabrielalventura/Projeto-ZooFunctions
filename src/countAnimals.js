6b054189e9d69128afbaeff5287bf8f0160f39d5
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-08-29 17:40:36 -0300

    Requisito 5 finalizado

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const totalAnimals = {};
+  if (animal === undefined) {
+    species.forEach((choosedAnimal) => {
+      totalAnimals[choosedAnimal.name] = choosedAnimal.residents.length;
+    });
+    return totalAnimals;
+  }
+  if (animal.sex === undefined) {
+    const seekedAnimal = species.find((oneAnimal) => oneAnimal.name === animal.specie).residents;
+    return seekedAnimal.length;
+  }
+  const searchingWSex = species.find((theAnimal) => theAnimal.name === animal.specie).residents;
+  const bySex = searchingWSex.filter((pet) => pet.sex === animal.sex);
+  return bySex.length;
 }
 
 module.exports = countAnimals;
