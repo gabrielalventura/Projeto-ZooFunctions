c05ed277c4e62aaad778940c6ddc704d69c13e0f
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-08-29 15:30:39 -0300

    Primeira tentativa de realização do requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,16 @@
 const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
-function isManager(id) {
-  // seu código aqui
+function isManager(employeeId) {
+  return employees.some((employee) => employee.id === employeeId && employee.managers === []);
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return employees.find((element) => element.managers.includes(managerId))
+    .map((element) => `${element.firstName} ${element.lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
