b9a513a0a30fc70db46c2d43ffdf83e9ae010ed5
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-08-30 15:50:47 -0300

    Iniciando o requisito 9

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,62 @@
 const data = require('../data/zoo_data');
+const { species, hours } = require('../data/zoo_data');
+
+function getSpecieSchedule(specie) {
+  return species.find((animal) => animal.name === specie).availability;
+}
+
+function catchSpecies(day) {
+  const inExhibition = species.filter((animal) => animal.availability.includes(day));
+  const specieOfTheDay = inExhibition.map((pet) => pet.name);
+  return specieOfTheDay;
+}
+
+function catchOfficeHour(day) {
+  return `Open from ${hours[day].open}am until ${hours[day].close}pm`;
+}
+
+const weekSchedule = {
+  Tuesday: {
+    officeHour: catchOfficeHour('Tuesday'),
+    exhibition: catchSpecies('Tuesday'),
+  },
+  Wednesday: {
+    officeHour: catchOfficeHour('Wednesday'),
+    exhibition: catchSpecies('Wednesday'),
+  },
+  Thursday: {
+    officeHour: catchOfficeHour('Thursday'),
+    exhibition: catchSpecies('Thursday'),
+  },
+  Friday: {
+    officeHour: catchOfficeHour('Friday'),
+    exhibition: catchSpecies('Friday'),
+  },
+  Saturday: {
+    officeHour: catchOfficeHour('Saturday'),
+    exhibition: catchSpecies('Saturday'),
+  },
+  Sunday: {
+    officeHour: catchOfficeHour('Sunday'),
+    exhibition: catchSpecies('Sunday'),
+  },
+  Monday: {
+    officeHour: 'CLOSED',
+    exhibition: 'The zoo will be closed!',
+  },
+};
 
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  if (species.map((pet) => pet.name)
+    .includes(scheduleTarget)) {
+    return getSpecieSchedule(scheduleTarget);
+  }
+  if (scheduleTarget === undefined) {
+    return weekSchedule;
+  }
+  if (Object.keys(hours).includes(scheduleTarget)) {
+    return catchSpecies(scheduleTarget);
+  }
 }
 
 module.exports = getSchedule;
