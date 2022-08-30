6d2a4212ddd061c2c141286958ff5dc816f5da7f
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-08-30 13:31:56 -0300

    Requisito 10 finalizado

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
+const { species, employees } = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+function getOldestFromFirstSpecies(employeeId) {
+  const responsible = employees.find((person) => person.id === employeeId);
+  const firstSpecies = responsible.responsibleFor[0];
+  const specie = species.find((pet) => pet.id === firstSpecies).residents;
+  const oldestFromSpecie = specie.sort((young, old) => old.age - young.age)[0];
+  return Object.values(oldestFromSpecie);
 }
 
 module.exports = getOldestFromFirstSpecies;
