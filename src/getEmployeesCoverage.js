a57ba8e9313d4a5645b03c9045992dcf29d18ae5
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-09-02 18:13:23 -0300

    Primeira tentativa de realização do requisito 11 - fail

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,17 +1,55 @@
 const data = require('../data/zoo_data');
 const { species, employees } = require('../data/zoo_data');
 
+function validatePerson(choosedData) {
+  const info = (Object.values(data))[0];
+  return employees.filter((worker) =>
+    worker.id === info || worker.firstName === info || worker.lastName === info);
+}
+
 function captureSpeciesName(worker) {
   const speciesId = employees.filter((person) => person.id === worker).responsibleFor;
-  let speciesForPerson = [];
+  const speciesForPerson = [];
   speciesId.forEach((captureId) => {
     const listOfSpecies = species.find((animal) => animal.id === captureId).name;
     speciesForPerson.push(listOfSpecies);
   });
   return speciesForPerson;
 }
-function getEmployeesCoverage() {
-  // seu código aqui
+
+function captureSpeciesLocation(worker) {
+  const speciesId = employees.filter((person) => person.id === worker).responsibleFor;
+  const locationsForSpecies = [];
+  speciesId.forEach((captureId) => {
+    const listOfSpecies = species.find((animal) => animal.id === captureId).location;
+    locationsForSpecies.push(listOfSpecies);
+  });
+  return locationsForSpecies;
 }
 
+function createObject(param) {
+  return {
+    id: param.id,
+    fullName: `${param.firstName} ${param.lastName}`,
+    species: captureSpeciesName(param.id),
+    locations: captureSpeciesLocation(param.id),
+  };
+}
+
+const withoutParam = employees.map((person) => ({
+  id: person.id,
+  fullName: `${person.firstName} ${person.lastName}`,
+  species: captureSpeciesName(person.id),
+  locations: captureSpeciesLocation(person.id),
+}));
+
+function getEmployeesCoverage(param) {
+  if (validatePerson(param) !== false) {
+    return createObject(param);
+  }
+  if (param === undefined) {
+    return withoutParam;
+  }
+  throw new Error('Informações inválidas');
+}
 module.exports = getEmployeesCoverage;
