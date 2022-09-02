1c3a76d47a137848f5e0ff5a40d95cbe2072de28
Gabriela Ventura <gabrielaleitev@gmail.com>
2022-09-02 17:16:08 -0300

    Iniciando o requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,5 +1,15 @@
 const data = require('../data/zoo_data');
+const { species, employees } = require('../data/zoo_data');
 
+function captureSpeciesName(worker) {
+  const speciesId = employees.filter((person) => person.id === worker).responsibleFor;
+  let speciesForPerson = [];
+  speciesId.forEach((captureId) => {
+    const listOfSpecies = species.find((animal) => animal.id === captureId).name;
+    speciesForPerson.push(listOfSpecies);
+  });
+  return speciesForPerson;
+}
 function getEmployeesCoverage() {
   // seu código aqui
 }
